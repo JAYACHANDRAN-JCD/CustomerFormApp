db38de30f06ba3cc95ca7c2837b721d15b53d556
jaichandranPD <jayachandranprofessional@gmail.com>
2025-01-12 23:21:17 +0530

    PWA setup

---
+++ rapiddrop/src/components/Backup.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Data from Backend</h1>
+      <ul>
+        <li v-for="item in data" :key="item.id">{{ item.date }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import api from "../services/api";
+  
+  export default {
+    data() {
+      return {
+        data: [],
+      };
+    },
+    created() {
+        console.log('Created')
+      api.get("WeatherForecast")
+        .then((response) => {
+          this.data = response.data;
+          console.log('Data:', this.data)
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  };
+  </script>
+  
---
+++ rapiddrop/src/components/DeliveryForm.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-app>
+        <v-img src="@/assets/RapidDrop.png"></v-img>
+    </v-app>
+</template>
